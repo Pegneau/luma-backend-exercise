3f37cf2fad67603fc332ab2789105253bdf96e40
Rodrigo Lourenzutti Pegneau <rodrigopegneau@gmail.com>
2026-03-30 19:08:45 +0200

    feat: Add fhir types

---
+++ src/types/fhir.ts
@@ -0,0 +1,83 @@
+// FHIR primitive aliases
+type FhirInstant = string;
+type FhirDateTime = string;
+type FhirCode = string;
+type FhirUri = string;
+
+// Shared types
+export interface Reference {
+  reference?: string;
+  type?: FhirUri;
+  display?: string;
+}
+
+export interface Coding {
+  system?: FhirUri;
+  version?: string;
+  code?: FhirCode;
+  display?: string;
+  userSelected?: boolean;
+}
+
+export interface CodeableConcept {
+  coding?: Coding[];
+  text?: string;
+}
+
+export interface Period {
+  start?: FhirDateTime;
+  end?: FhirDateTime;
+}
+
+// Enums (FHIR-aligned)
+export type AppointmentStatus =
+  | "proposed"
+  | "pending"
+  | "booked"
+  | "arrived"
+  | "fulfilled"
+  | "cancelled"
+  | "noshow"
+  | "entered-in-error"
+  | "checked-in"
+  | "waitlist";
+
+export type ParticipantRequired = "required" | "optional" | "information-only";
+
+export type ParticipantStatus =
+  | "accepted"
+  | "declined"
+  | "tentative"
+  | "needs-action";
+
+// Appointment participant
+export interface AppointmentParticipant {
+  type?: CodeableConcept[];
+  actor?: Reference;
+  required?: ParticipantRequired;
+  status: ParticipantStatus;
+  period?: Period;
+}
+
+// Main resource
+//https://hl7.org/fhir/R4/appointment.html json sample
+export interface FhirAppointment {
+  resourceType: "Appointment";
+  id?: string;
+  status: AppointmentStatus;
+
+  serviceCategory?: CodeableConcept[];
+  serviceType?: CodeableConcept[];
+  specialty?: CodeableConcept[];
+  appointmentType?: CodeableConcept;
+  reasonCode?: CodeableConcept[];
+
+  start?: FhirInstant;
+  end?: FhirInstant;
+  minutesDuration?: number;
+
+  comment?: string;
+  requestedPeriod?: Period[];
+
+  participant: AppointmentParticipant[];
+}
